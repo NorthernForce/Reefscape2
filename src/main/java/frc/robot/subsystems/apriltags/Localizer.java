6b294a97eefd76c05a5bf6b2caf03a688a747d67
cml1010101 <cml1010101@gmail.com>
2025-03-30 09:10:35 -0400

    Add functions to get whether has had recent pose estimate

--- src/main/java/frc/robot/subsystems/apriltags/Localizer.java
+++ src/main/java/frc/robot/subsystems/apriltags/Localizer.java
@@ -30,13 +30,18 @@ public class Localizer extends SubsystemBase
     private final PhotonPoseEstimator centerPoseEstimator = new PhotonPoseEstimator(FieldConstants.kField,
             PoseStrategy.CONSTRAINED_SOLVEPNP, CameraConstants.kCenterCameraTransform);
     private EstimatedRobotPose[] estimatedPoses = new EstimatedRobotPose[0];
+    private Pose2d[] frontLeftPoses = new Pose2d[0];
+    private Pose2d[] frontRightPoses = new Pose2d[0];
+    private Pose2d[] centerPoses = new Pose2d[0];
     private final VisionSystemSim sim;
+    private final Timer timeSinceLastEstimate;
 
     public Localizer()
     {
         frontLeftPoseEstimator.setMultiTagFallbackStrategy(PoseStrategy.CLOSEST_TO_REFERENCE_POSE);
         frontRightPoseEstimator.setMultiTagFallbackStrategy(PoseStrategy.CLOSEST_TO_REFERENCE_POSE);
         centerPoseEstimator.setMultiTagFallbackStrategy(PoseStrategy.CLOSEST_TO_REFERENCE_POSE);
+        timeSinceLastEstimate = new Timer();
         if (RobotBase.isSimulation())
         {
             sim = new VisionSystemSim("main-vision");
@@ -69,23 +74,70 @@ public class Localizer extends SubsystemBase
     public void periodic()
     {
         ArrayList<EstimatedRobotPose> poses = new ArrayList<>();
+        ArrayList<Pose2d> frontLeftPoses = new ArrayList<>();
+        ArrayList<Pose2d> frontRightPoses = new ArrayList<>();
+        ArrayList<Pose2d> centerPoses = new ArrayList<>();
         frontRightCamera.getAllUnreadResults().forEach(result ->
         {
-            frontRightPoseEstimator.update(result).ifPresent(pose -> poses.add(pose));
+            frontRightPoseEstimator.update(result).ifPresent(pose ->
+            {
+                poses.add(pose);
+                frontRightPoses.add(pose.estimatedPose.toPose2d());
+            });
         });
         frontLeftCamera.getAllUnreadResults().forEach(result ->
         {
-            frontLeftPoseEstimator.update(result).ifPresent(pose -> poses.add(pose));
+            frontLeftPoseEstimator.update(result).ifPresent(pose ->
+            {
+                poses.add(pose);
+                frontLeftPoses.add(pose.estimatedPose.toPose2d());
+            });
         });
         centerCamera.getAllUnreadResults().forEach(result ->
         {
-            centerPoseEstimator.update(result).ifPresent(pose -> poses.add(pose));
+            centerPoseEstimator.update(result).ifPresent(pose ->
+            {
+                poses.add(pose);
+                centerPoses.add(pose.estimatedPose.toPose2d());
+            });
         });
         estimatedPoses = poses.toArray(new EstimatedRobotPose[0]);
+        this.frontLeftPoses = frontLeftPoses.toArray(new Pose2d[0]);
+        this.frontRightPoses = frontRightPoses.toArray(new Pose2d[0]);
+        this.centerPoses = centerPoses.toArray(new Pose2d[0]);
+        if (estimatedPoses.length > 0)
+        {
+            timeSinceLastEstimate.restart();
+        }
     }
 
     public EstimatedRobotPose[] getEstimatedPoses()
     {
         return estimatedPoses;
+    }
+
+    public Pose2d[] getFrontLeftPoses()
+    {
+        return frontLeftPoses;
+    }
+
+    public Pose2d[] getFrontRightPoses()
+    {
+        return frontRightPoses;
+    }
+
+    public Pose2d[] getCenterPoses()
+    {
+        return centerPoses;
+    }
+
+    public double getTimeSinceLastEstimate()
+    {
+        return timeSinceLastEstimate.get();
+    }
+
+    public boolean hasHadRecentEstimate()
+    {
+        return timeSinceLastEstimate.hasElapsed(0.5) && timeSinceLastEstimate.isRunning();
     }
 }
